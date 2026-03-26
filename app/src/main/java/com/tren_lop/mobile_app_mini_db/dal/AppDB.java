877fae3c76cd328e83951a11a0ed312b9f3e0080
Pduc96421 <pduc96421@gmail.com>
2026-03-26 19:42:13 +0700

    base

---
+++ app/src/main/java/com/tren_lop/mobile_app_mini_db/dal/AppDB.java
@@ -0,0 +1,7 @@
+package com.tren_lop.mobile_app_mini_db.dal;
+
+
+import androidx.room.RoomDatabase;
+
+public abstract class AppDB extends RoomDatabase {
+}
---
+++ app/src/main/java/com/tren_lop/mobile_app_mini_db/dal/DAO.java
@@ -0,0 +1,8 @@
+package com.tren_lop.mobile_app_mini_db.dal;
+
+import androidx.room.Dao;
+
+@Dao
+public interface DAO {
+
+}
