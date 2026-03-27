e1bd5e67f276629febfdc867f47bb80521cc1252
Tran Duc Phuong <ducphuong050904@gmail.com>
2026-03-27 16:01:39 +0700

    Add adapters

---
+++ app/src/main/java/com/tren_lop/mobile_app_mini_db/ui/TheaterAdapter.java
@@ -0,0 +1,49 @@
+package com.example.minipro.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.minipro.R;
+import com.example.minipro.data.entity.Theater;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheaterAdapter extends RecyclerView.Adapter<TheaterAdapter.TheaterViewHolder> {
+    private List<Theater> theaters = new ArrayList<>();
+
+    public void setTheaters(List<Theater> theaters) {
+        this.theaters = theaters;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public TheaterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_theater, parent, false);
+        return new TheaterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TheaterViewHolder holder, int position) {
+        Theater theater = theaters.get(position);
+        holder.tvName.setText(theater.name);
+        holder.tvLocation.setText(theater.location);
+    }
+
+    @Override
+    public int getItemCount() {
+        return theaters.size();
+    }
+
+    static class TheaterViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvLocation;
+        public TheaterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+        }
+    }
+}
